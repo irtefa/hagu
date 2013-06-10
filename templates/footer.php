fb5e90c035833c580c74547b6fc9a641ceb0437a
Mohd Irtefa <irtefa1@illinois.edu>
2013-06-09 20:35:55 -0700

    added links to the footer

--- templates/footer.php
+++ templates/footer.php
@@ -1,3 +1,12 @@
     </div>
+    <hr>
+    <footer id="footer">
+        <div class="links">
+            <a href="http://irtefa.com/thoughts">Blog</a>
+            <a href="http://github.com/irtefa">Github</a>
+        </div>
+        <p>Created by <a href="http://irtefa.com">Mohd Irtefa.</a></p>
+        <p>Powered by <a href="http://yelp.com">Yelp</a></p>
+    </footer>
 </body>
 </html>
