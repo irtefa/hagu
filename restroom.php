b0102a40338d28c288102dff98c6d7beee140d9b
Mohd Irtefa <irtefa1@illinois.edu>
2013-06-14 23:38:57 -0700

    added text form and male/female gender selection

--- restroom.php
+++ restroom.php
@@ -1,4 +1,7 @@
 <?php
+require 'templates/header.php';
 $id = $_GET['id'];
 echo($id);
+require 'review.php';
+require 'templates/footer.php';
 ?>
---
+++ review.php
@@ -0,0 +1,10 @@
+<br>
+<textarea rows="3" id="review" class="input-xlarge"></textarea>
+<br>
+<select id="gender">
+    <option>Male</option>
+    <option>Female</option>
+</select>
+<br>
+<button id="submit-review" class="btn btn-primary">Review</button>
+
